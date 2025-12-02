77323948fc39725d475cbc4878bc35055ca6bf3e
Azuhaha <15370082450@163.com>
2025-12-02 11:05:36 +0800

    FVM1

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 
-from FVM.Class.RegionClass import Region
+from Class.RegionClass import Region
 from ReadOpenFoamFiles import *
 from DefineMomentumEquation import *
 from DefineContinuityEquation import *
@@ -26,6 +26,7 @@ definemomentumequation(region)
 #定义region.model.p; 初始化mdot_f=rho_f*U_f*Sf; 初始化DU1 DU2 DU3 DUT1 DUT2 DUT3 pp为0值
 definecontinuityequation(region)
 
+#开始运行
 runcase(region)
 
 print('finished')
